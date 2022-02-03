90c7214edb564db2580b1fc75dd2589dd992abee
nessaj9698 <timoshin.i@list.ru>
2022-02-03 14:45:38 +0300

    Updates

---
+++ precache-manifest.d3b6f51cc6fa8b0ac4872be2495ee8c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4516e16ec7ac5503cf4ac036b25d6d",
+    "url": "/TimePlanner/index.html"
+  },
+  {
+    "revision": "26f2e6ba06cd157e8e98",
+    "url": "/TimePlanner/static/css/2.e3a89a79.chunk.css"
+  },
+  {
+    "revision": "977efd9f8359af4d7723",
+    "url": "/TimePlanner/static/css/main.0ad9a076.chunk.css"
+  },
+  {
+    "revision": "26f2e6ba06cd157e8e98",
+    "url": "/TimePlanner/static/js/2.c0310317.chunk.js"
+  },
+  {
+    "revision": "834b764bd40e6353fbde506dc6921fd7",
+    "url": "/TimePlanner/static/js/2.c0310317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f1a7584aed2098fb4b",
+    "url": "/TimePlanner/static/js/3.4235dc93.chunk.js"
+  },
+  {
+    "revision": "977efd9f8359af4d7723",
+    "url": "/TimePlanner/static/js/main.aaf6d25d.chunk.js"
+  },
+  {
+    "revision": "bd20c3eb1da5ee9504a8",
+    "url": "/TimePlanner/static/js/runtime-main.2c5b557a.js"
+  },
+  {
+    "revision": "f6c770b6b24683b5714162da635e66e3",
+    "url": "/TimePlanner/static/media/4-04.f6c770b6.svg"
+  },
+  {
+    "revision": "4388916009d9c1bac014748ed73acf80",
+    "url": "/TimePlanner/static/media/logo.43889160.svg"
+  }
+]);
