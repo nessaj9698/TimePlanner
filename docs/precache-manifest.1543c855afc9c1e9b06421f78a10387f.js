fe3aa5d0a46fd1ccd370ee17c438805a64646d27
Vadim <andrvalid@yandex.ru>
2023-07-05 21:48:25 +0500

    fix some style bugs

---
+++ docs/precache-manifest.1543c855afc9c1e9b06421f78a10387f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4240696d4d717d5e9cf84775cf7a92",
+    "url": "/TimePlanner/index.html"
+  },
+  {
+    "revision": "8fc39098dfa8cd2c8063",
+    "url": "/TimePlanner/static/css/2.989ce313.chunk.css"
+  },
+  {
+    "revision": "99baa85368334697628f",
+    "url": "/TimePlanner/static/css/main.492b3c2c.chunk.css"
+  },
+  {
+    "revision": "8fc39098dfa8cd2c8063",
+    "url": "/TimePlanner/static/js/2.38b19480.chunk.js"
+  },
+  {
+    "revision": "4dcddd89528135405f4faeeb0779c432",
+    "url": "/TimePlanner/static/js/2.38b19480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f1a7584aed2098fb4b",
+    "url": "/TimePlanner/static/js/3.4235dc93.chunk.js"
+  },
+  {
+    "revision": "99baa85368334697628f",
+    "url": "/TimePlanner/static/js/main.9d8ec640.chunk.js"
+  },
+  {
+    "revision": "bd20c3eb1da5ee9504a8",
+    "url": "/TimePlanner/static/js/runtime-main.2c5b557a.js"
+  },
+  {
+    "revision": "f6c770b6b24683b5714162da635e66e3",
+    "url": "/TimePlanner/static/media/4-04.f6c770b6.svg"
+  },
+  {
+    "revision": "4388916009d9c1bac014748ed73acf80",
+    "url": "/TimePlanner/static/media/logo.43889160.svg"
+  }
+]);
