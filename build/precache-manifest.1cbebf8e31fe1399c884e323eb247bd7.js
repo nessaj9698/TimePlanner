340804e54ab29eec948aef9ddcdc8e9735e2942a
Vadim <andrvalid@yandex.ru>
2023-07-28 21:23:49 +0500

    add build folder for gh-pages

---
+++ build/precache-manifest.1cbebf8e31fe1399c884e323eb247bd7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068f8b76373f2427a0506329cda89f59",
+    "url": "/TimePlanner/index.html"
+  },
+  {
+    "revision": "770885666989a5874b7b",
+    "url": "/TimePlanner/static/css/2.989ce313.chunk.css"
+  },
+  {
+    "revision": "5aaa6ea346d4f8ef1a2e",
+    "url": "/TimePlanner/static/css/main.83b47cff.chunk.css"
+  },
+  {
+    "revision": "770885666989a5874b7b",
+    "url": "/TimePlanner/static/js/2.c5ba4fba.chunk.js"
+  },
+  {
+    "revision": "4dcddd89528135405f4faeeb0779c432",
+    "url": "/TimePlanner/static/js/2.c5ba4fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f1a7584aed2098fb4b",
+    "url": "/TimePlanner/static/js/3.4235dc93.chunk.js"
+  },
+  {
+    "revision": "5aaa6ea346d4f8ef1a2e",
+    "url": "/TimePlanner/static/js/main.55e0e4c7.chunk.js"
+  },
+  {
+    "revision": "bd20c3eb1da5ee9504a8",
+    "url": "/TimePlanner/static/js/runtime-main.2c5b557a.js"
+  },
+  {
+    "revision": "dff83f42e10d8d0c7412ba2253857ffc",
+    "url": "/TimePlanner/static/media/4-04.dff83f42.svg"
+  },
+  {
+    "revision": "4388916009d9c1bac014748ed73acf80",
+    "url": "/TimePlanner/static/media/logo.43889160.svg"
+  }
+]);
